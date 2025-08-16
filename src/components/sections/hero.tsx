372b18363f4ef5db3b33ed55069b3ef99533938d
Vikram Gupta <vikramgupta1704@gmail.com>
2025-08-16 14:52:02 +0530

    .

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -83,7 +83,7 @@ const HeroSection = () => {
               <div className="mt-8 md:ml-2 flex flex-col gap-3">
                 <Link
                   href={
-                    "https://drive.google.com/file/d/1IsVk0-7e2AA3Lw3DPvEMrD3tAihcIB51/view"
+                    "https://drive.google.com/file/d/14UkjMS_jaQVeqEd3_of-T7Puohmp1ZVX/view"
                   }
                   target="_blank"
                   className="flex-1"
